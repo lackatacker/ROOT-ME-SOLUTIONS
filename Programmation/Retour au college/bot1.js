82a265d98d3e2ea01c958a1353b0844491400269
lackatacker <lackatacker@DESKTOP-N3V1CK4>
2022-05-29 12:47:50 +0100

    first commit

---
+++ Programmation/Retour au college/bot1.js
@@ -0,0 +1,17 @@
+var ircClient = require('node-irc');
+var client = new ircClient('irc.root-me.org', 6667, 'yasin', 'SomeFullname');
+client.on('ready', function () {
+    client.join('#root-me_challenge');
+    client.say('Candy', "!ep1")
+  });
+client.on('PRIVMSG',function(data){
+    const myArray = data.message.split(" / ")
+    const number = Math.sqrt( myArray[0])*myArray[1]
+    const resp = "!ep1 -rep "+number.toFixed(2)
+    console.log(resp)
+    client.say(data.sender, resp)
+})
+client.on('PRIVMSG', function(data){
+    console.log(data.message)
+})
+client.connect()
